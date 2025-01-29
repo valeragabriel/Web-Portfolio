dc61a1924c01bb33bc8b9d80f4d95683e42fbb17
Gabriel Gobbo Araújo Valera <gobbo.gabriel@grad.ufsc.br>
2025-01-28 21:33:37 -0300

    adicionando database e o configurando

--- tests/integration/ap1/v1/status/get.test.ts
+++ tests/integration/ap1/v1/status/get.test.ts
@@ -1,9 +1,12 @@
 import fetchMock from "jest-fetch-mock";
+import database  from "../../../../../infra/database.ts";
 
 fetchMock.enableMocks(); 
 
 test("GET to /api/v1/status should return 200", async () => {
   fetchMock.mockResponseOnce(JSON.stringify({ status: "ok" }));
+  const result = await database.query("SELECT 1 + 1");  
+  console.log(result);
 
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
