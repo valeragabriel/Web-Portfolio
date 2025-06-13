16b47701cc6ac318e319c6313f3863ba40ffe463
Gabriel Gobbo Araújo Valera <115488033+valeragabriel@users.noreply.github.com>
2025-06-13 14:40:29 +0000

    feat: add new method for errors

--- infra/error.ts
+++ infra/error.ts
@@ -1,13 +1,53 @@
 export class InternalServerError extends Error {
   action: string;
   statusCode: number;
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super('Internal Server Error', {
       cause,
     });
     this.name = 'InternalServerError';
     this.action = 'Contact with suport team';
-    this.statusCode = 500;
+    this.statusCode = statusCode;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  action: string;
+  statusCode: number;
+  constructor({ cause, message }) {
+    super(message || 'Service unvailable', {
+      cause,
+    });
+    this.name = 'ServiceError';
+    this.action = 'Check Service available ';
+    this.statusCode = 503;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowed extends Error {
+  action: string;
+  statusCode: number;
+  constructor() {
+    super('Method Not Allowed');
+    this.name = 'MethodNotAllowed';
+    this.action = 'Try another method, this method not exist in system for this url';
+    this.statusCode = 405;
   }
   toJSON() {
     return {
