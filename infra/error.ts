959b1cedc5d930046154c93cbf78f58c15f271e0
Gabriel Gobbo Araújo Valera <115488033+valeragabriel@users.noreply.github.com>
2025-06-16 14:14:12 +0000

    feat: changes in controller and new error methods

--- infra/error.ts
+++ infra/error.ts
@@ -40,6 +40,28 @@ export class ServiceError extends Error {
   }
 }
 
+export class ValidationError extends Error {
+  action: string;
+  statusCode: number;
+  constructor({ cause, message, action }) {
+    super(message || 'Um erro de validação ocorreu.', {
+      cause,
+    });
+    this.name = 'ValidationError';
+    this.action = action || 'Ajuste os dados enviados e tente novamente.';
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowed extends Error {
   action: string;
   statusCode: number;
@@ -49,6 +71,69 @@ export class MethodNotAllowed extends Error {
     this.action = 'Try another method, this method not exist in system for this url';
     this.statusCode = 405;
   }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class NotFoundError extends Error {
+  action: string;
+  statusCode: number;
+  constructor({ cause, message, action }) {
+    super(message || 'Não foi possível encontrar este recurso no sistema.', {
+      cause,
+    });
+    this.name = 'NotFoundError';
+    this.action = action || 'Verifique se os parâmetros enviados na consulta estão certos.';
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+export class UnauthorizedError extends Error {
+  action: string;
+  statusCode: number;
+  constructor({ cause, message, action }) {
+    super(message || 'Usuário não autenticado.', {
+      cause,
+    });
+    this.name = 'UnauthorizedError';
+    this.action = action || 'Faça novamente o login para continuar.';
+    this.statusCode = 401;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  action: string;
+  statusCode: number;
+  constructor() {
+    super('Método não permitido para este endpoint.');
+    this.name = 'MethodNotAllowedError';
+    this.action = 'Verifique se o método HTTP enviado é válido para este endpoint.';
+    this.statusCode = 405;
+  }
+
   toJSON() {
     return {
       name: this.name,
